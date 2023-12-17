e0816ebfb0ce5beacf6f7d5332dc1360f42921a6
Kanishk <ksethi194@gmail.com>
2023-12-16 19:33:43 -0800

    safety commit

--- sandbox.py
+++ sandbox.py
@@ -1,40 +1,21 @@
+'''
+
+The purpose of this file is for testing and playing around with matplotlib, numpy, and ML principles
+
+'''
+
+
+# Libraries that are used often in Machine Learning 
+
+# This first one is matplotlib, it is used for plotting functions
 import matplotlib.pyplot as plt
+# This one is a math library that allows us to do various mathematical tasks
 import numpy as np
 
-# # Define a linear function with just one input, x
-# def linear_function_1D(x,beta,omega):
-#   y = omega*x+beta
-#   return y
+x1 = np.arange(0,10,.1)
+x2= np.arange(0,10,.1)
 
-
-# # Plot the 1D linear function
-
-# # Define an array of x values from 0 to 10 with increments of 0.01
-# # https://numpy.org/doc/stable/reference/generated/numpy.arange.html
-
-# x = np.arange(0.0,10.0, 0.01)
-# x1 = np.arange (0.0,10.0, 0.01)
-
-# # Compute y using the function you filled in above
-# beta = 10.0; omega = -2.0
-# y = linear_function_1D(x,beta,omega)
-
-# beta1=0; omega1 = 2.0 
-# y1 = linear_function_1D(x1, beta1, omega1)
-
-# # Plot this function
-# fig, ax = plt.subplots() 
-# ax.plot(x,y,'r-')
-# ax.plot(x1,y1,'b-')
-# ax.set_ylim([0,10]); ax.set_xlim([0,10])
-# ax.set_xlabel('x'); ax.set_ylabel('y')
-# plt.show()
-
-# Make 2D array of x and y points
-x1 = np.arange(0.0, 10.0, 2)
-x2 = np.arange(10.0, 20.0, 2)
-x1,x2 = np.meshgrid(x1,x2) 
-
+x1,x2 = np.meshgrid(x1,x2)
 
 print(x1)
 print(x2)
